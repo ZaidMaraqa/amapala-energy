e35854d898dcdc70a719d8e6d25003fdb8f75079
ZaidMaraqa <zaidmaraqa10@gmail.com>
2024-08-16 20:20:31 +0300

    blog page

--- src/app/blog/[id]/page.tsx
+++ src/app/blog/[id]/page.tsx
@@ -1,53 +1,61 @@
 "use client";
-import React from "react";
+
+import "@fontsource/lexend";
+
 import {
   Box,
-  Heading,
-  Text,
-  Image,
-  Stack,
+  Divider,
   Flex,
+  Heading,
+  Image,
   Spinner,
+  Stack,
+  Text,
 } from "@chakra-ui/react";
-import Footer from "@/components/footer";
 
+import BlogList from "@/components/blogs/blogsList";
+import BlogNewsLetter from "@/components/blogs/blogNewsLetter";
+import Footer from "@/components/footer";
 import NavBar from "@/components/navbar";
-import getBlog from "@/hooks/getBlog";
+import React from "react";
 import { formatDate } from "@/utils";
+import getBlog from "@/hooks/getBlog";
 
 const BlogOverview = ({ params }: { params: { id: string } }) => {
   const id = params.id
   const { data, isLoading, error } = getBlog(id)
 
+  console.log(data)
+
   if (isLoading) return <Flex h={'100vh'} w={'100vw'} justifyContent={'center'} alignItems={'center'}><Spinner thickness='0.25rem' speed='0.55s' emptyColor='gray.200' color='primeBlue' size='xl' /></Flex>;
   if (error) return <div>Failed to load data</div>;
 
   return (
-    <>
+    <Flex direction={'column'} h={'100vh'}>
       <NavBar />
-      <Box bg="" px={'2rem'} py={'1rem'}>
-        <Stack direction={{ base: "column", lg: "row" }} spacing={10}>
-          <Box flex="3">
-            <Text fontSize="sm" color="gray.500">
-              Energy &gt; Insights
-            </Text>
-            <Heading fontSize="3xl" my={4}>
-              {data.title}
-            </Heading>
-            <Text color="gray.500" mb={4}>
-              By {data.author} <br />
-              {formatDate(data.date)} &middot; {data.readingTime} minute read
-            </Text>
-            <Image src={data.image} alt="Main Image" mb={4} />
-            <Text mb={4}>
-              {data.content}
-            </Text>
-            </Box>
-
-        </Stack>
-      </Box>
+      <Flex flex={1} direction={'column'} gap={'2rem'}>
+          <Flex direction={'row'} p={'2rem'}>
+            <Image src={data.image}  w={'350px'} h={'350px'} alt="blog related pic" borderRadius={'.5rem'} />
+            <Flex direction={'column'} gap={'1rem'} px={'1rem'} py={'.5rem'} justifyContent={'space-between'}>
+              <Flex direction={'row'} alignItems={'center'} gap={'.5rem'} mt={'1rem'}>
+                <Box bg={'#00842A'} p={'.5rem'} borderRadius={'.25rem'}>
+                  <Text fontWeight={600} fontSize={'1rem'} color={'white'}>{data.category}</Text>
+                </Box>
+                <Text fontSize={'20px'} fontWeight={600}>{data.readingTime} min read</Text>
+              </Flex>
+              <Heading fontWeight={600} fontSize={'4.375rem'} >{data.title}</Heading>
+              <Text fontWeight={600} fontSize={'1rem'} color={'#696D70'} mb={'1rem'}>{formatDate(data.date)}</Text>
+            </Flex>
+          </Flex>
+          <Text fontFamily="'Lexend', sans-serif" fontWeight={500} fontSize={'1.25rem'} color={'#696D70'} px={'2rem'}>{data.content}</Text>
+          <BlogList isSpecficBlog={true} />
+          <Box p={'2rem'}>
+            <Divider colorScheme={'blue'} orientation="horizontal" size={'lg'} />
+          <BlogNewsLetter />
+          </Box>
+      </Flex>
       <Footer />
-    </>
+    </Flex>
   );
 };
 
--- src/components/blogs/blogsList.tsx
+++ src/components/blogs/blogsList.tsx
@@ -9,7 +9,11 @@ import getBlogs from "@/hooks/getAllBlogs";
 import { useDebounce } from "use-debounce";
 import { useState } from "react";
 
-const BlogList = () => {
+interface BlogListProps {
+  isSpecficBlog?: boolean;
+}
+
+const BlogList = ({ isSpecficBlog }: BlogListProps) => {
   const [currentPage, setCurrentPage] = useState<number>(1);
   const [search, setSearch] = useState<string>('');
   const [category, setCategory] = useState<string>('');
@@ -25,7 +29,7 @@ const BlogList = () => {
 
   const handleSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setSearch(e.target.value);
-    setCurrentPage(1); 
+    setCurrentPage(1);
   };
 
   if (isLoading) return <Flex h={'10rem'} justifyContent={'center'} alignItems={'center'}><Spinner thickness='0.25rem' speed='0.55s' emptyColor='gray.200' color='primeBlue' size='xl' /></Flex>;
@@ -36,31 +40,41 @@ const BlogList = () => {
 
   return (
     <Flex direction={'column'} gap={'1.5rem'} bg="primeGray" py={'5rem'} px={['.5rem', '3rem', '3rem', '3rem']}>
-      <Flex direction={'row'} justifyContent={'space-between'} alignItems={'center'}>
-        <Flex direction={'column'} gap={'1.5rem'} flex={2}>
-          <Text fontFamily="'Lexend', sans-serif" fontSize={'1.5rem'} fontWeight={500} color={'#202122'}>
-            News and Analysis
-          </Text>
-          <Text color={'primeBlue'} fontWeight={600} fontSize={['1.375rem', '2.775rem', '3.575rem', '4.375rem', '5.375rem']}>
-            Discover <Text as={'span'} color={'black'}>The </Text>
-            <Text as={'span'} color={'black'}>Latest</Text> <br />Insights
+      {!isSpecficBlog ? (
+        <Flex direction={'row'} justifyContent={'space-between'} alignItems={'center'}>
+          <Flex direction={'column'} gap={'1.5rem'} flex={2}>
+            <Text fontFamily="'Lexend', sans-serif" fontSize={'1.5rem'} fontWeight={500} color={'#202122'}>
+              News and Analysis
+            </Text>
+            <Text color={'primeBlue'} fontWeight={600} fontSize={['1.375rem', '2.775rem', '3.575rem', '4.375rem', '5.375rem']}>
+              Discover <Text as={'span'} color={'black'}>The </Text>
+              <Text as={'span'} color={'black'}>Latest</Text> <br />Insights
+            </Text>
+          </Flex>
+
+          <Flex direction={'row'} gap={'.5rem'} flex={1}>
+            <Input
+              placeholder={`Search articles... (${totalPosts} posts)`}
+              value={search}
+              onChange={handleSearchChange}
+              borderColor="primeBlue"
+              borderRadius={0}
+              _placeholder={{ color: '#484848', fontSize: '.85rem', fontWeight: 400 }}
+              _hover={'none'}
+              display={'flex'}
+              borderWidth={'.125rem'}
+            />
+            <FilterPopover setCountry={setCountry} setCategory={setCategory} />
+          </Flex>
+        </Flex>
+      ) : (
+        <Flex justifyContent="center" alignItems="center" py="2rem">
+          <Text fontFamily="'Lexend', sans-serif" fontSize="2rem" fontWeight={600} color="primeBlue">
+            Keep exploring
           </Text>
         </Flex>
-        <Flex direction={'row'} gap={'.5rem'} flex={1}>
-          <Input
-            placeholder={`Search articles... (${totalPosts} posts)`}
-            value={search}
-            onChange={handleSearchChange}
-            borderColor="primeBlue"
-            borderRadius={0}
-            _placeholder={{ color:'#484848', fontSize: '.85rem', fontWeight: 400 }}
-            _hover={ 'none' }
-            display={'flex'}
-            borderWidth={'.125rem'}
-          />
-          <FilterPopover setCountry={setCountry} setCategory={setCategory} />
-        </Flex>
-      </Flex>
+      )}
+
       <SimpleGrid columns={[1, 1, 2, 3, 3]} spacing={10} mt={'1rem'}>
         {data?.blogs.map((blog: any, index: number) => (
           <ServiceCard
